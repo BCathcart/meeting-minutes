7c8ce21db7bda44ce68cbf0235e04cb66e69a172
Brennan <brennancathcart@gmail.com>
2019-01-26 19:14:38 -0800

    Working on app new meeting screen

--- react-native-app/meeting-minutes/navigation/AppNavigator.js
+++ react-native-app/meeting-minutes/navigation/AppNavigator.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
+//import MainTabNavigator from './MainTabNavigator';
+import HomeScreen from '../screens/HomeScreen';
+import MeetingMenuScreen from '../screens/MeetingMenuScreen';
+
+//const AppStack = createStackNavigator({ Home: HomeScreen, NewMeeting: LinksScreen});
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  // Main: MainTabNavigator,
+  // AuthLoading: AuthLoadingScreen,
+  // App: AppStack,
+  // Auth: AuthStack,
+  //Main: AppStack
+  Home: HomeScreen, 
+  Continue: MeetingMenuScreen
 }));
