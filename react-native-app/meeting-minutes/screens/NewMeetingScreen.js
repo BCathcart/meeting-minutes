21950342cc611c0c9f5c55b5b2d3ae7613328a27
Nandini Bulusu <nanbul785@gmail.com>
2019-01-26 23:36:26 -0800

    added missing files

---
+++ react-native-app/meeting-minutes/screens/NewMeetingScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+
+export default class NewMeetingScreen extends React.Component {
+static navigationOptions = {
+  header: null
+};
+
+render() {
+  return (
+    <View style={styles.container}>
+
+    <Text style={styles.meetingCode}>Your meeting code is: </Text>
+
+    <TouchableOpacity onPress={this._onPressOkay} style={styles.okayButton}>
+    <Image source={require('../assets/images/button_start-meeting.png')} />
+    </TouchableOpacity>
+
+    <Text style={styles.textNote}> Share this code to have others join your meeting. </Text>
+
+    </View>
+  );
+}
+
+_onPressOkay = () => {
+  this.props.navigation.navigate('Blah');
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F1F1F2',
+  },
+  meetingCode: {
+    position: 'absolute',
+    top: 100,
+    fontSize: 44,
+    fontWeight:'bold',
+    alignSelf: 'center',
+    textAlign: 'center'
+  },
+  textNote: {
+    position: 'absolute',
+    bottom: 200,
+    paddingVertical: 20,
+    alignSelf: 'center'
+  },
+  okayButton: {
+    position: 'absolute',
+    bottom: 120,
+    paddingVertical: 20,
+    alignSelf: 'center',
+  }
+});
